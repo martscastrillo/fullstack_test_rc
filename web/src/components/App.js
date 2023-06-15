67671cadbe30faa60e605383c3fbe4c885ddabab
martscastrillo <mart.castrillo@gmail.com>
2023-06-15 09:27:30 +0200

    add counter

--- web/src/components/App.js
+++ web/src/components/App.js
@@ -10,9 +10,10 @@ const App = () => {
 		value1: "holi",
 		value2: "holita",
 	});
-	const [dataArray, setDataArray] = useState([]);
+	const [contador, setContador] = useState(1);
+	/* const [dataArray, setDataArray] = useState([]);
 	const [inputValues, setInputValues] = useState({});
-	const [result, setResult] = useState(0);
+	const [result, setResult] = useState(0); */
 	const sendFormApi = (data) => {
 		apiData.sendFormApi(data).then((response) => {
 			if (response) {
@@ -24,67 +25,47 @@ const App = () => {
 	const handleSubmit = (ev) => {
 		ev.preventDefault();
 		sendFormApi(dataForm);
-		
+
 	};
-	
+	const [inputs, setInputs] = useState([]);
+
+	const agregarInput = () => {
+		setInputs([...inputs, '']);
+		setContador(contador + 1);
+	  };
+
+	const handleChange = (index, event) => {
+		const nuevosInputs = [...inputs];
+		nuevosInputs[index] = event.target.value;
+		setInputs(nuevosInputs);
+	};
 	return (
 		<div className="superbox">
 			<h1 className="superbox__h1">full stack test</h1>
 			<form action="" className="superbox__h1" onSubmit={handleSubmit}>
-				<fieldset className="superbox__form__fieldset">
+				
+
+				{inputs.map((value, index) => (
+					<fieldset className="superbox__form__fieldset">
 					<label htmlFor="value1">value 1</label>
 					<input
-						id="value1"
-						placeholder="value 1"
-						name="input1"
-						value={inputValues.input1 || ""}
+						id={`value${contador}`}
+						placeholder={`value ${index}`}
+						key={index}
+						value={value}
+						onChange={(event) => handleChange(index, event)}
+					/>
+				</fieldset>
 					
-					/>
-				</fieldset>
-				<fieldset className="superbox__form__fieldset">
-					<label htmlFor="value2">value 2</label>
-					<input
-						id="value2"
-						placeholder="value 2"
-						name="input2"
-						value={inputValues.input2 || ""}
-			
-					/>
-				</fieldset>
-				<fieldset className="superbox__form__fieldset">
-					<label htmlFor="value3">value 3</label>
-					<input
-						type="text"
-						id="value3"
-						placeholder="value 3"
-						name="input3"
-						value={inputValues.input3 || ""}
-					
-					/>
-				</fieldset>
-				<fieldset className="superbox__form__fieldset">
-					<label htmlFor="">value 4</label>
-					<input
-						type="text"
-						placeholder="value 4"
-						id="value4"
-						name="input4"
-						value={inputValues.input4 || ""}
-					/>
-				</fieldset>
-				<div className="superbox__form__extradiv">
-					<input
-						className="superbox__form__submit"
-						type="submit"
-						value="Submit"
-					/>
-
-				</div>
+				))}
+				<button type="button" onClick={agregarInput}>
+					Añadir Input
+				</button>
 			</form>
 
 			<div className="superbox__results">
-				<h2>history results: {`${result}`}</h2>
-			
+				{/* 				<h2>history results: {`${result}`}</h2>
+ */}
 			</div>
 		</div>
 	);
